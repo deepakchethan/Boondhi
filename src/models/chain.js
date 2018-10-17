f413ea6ce99856955d4a8ecb1b04aa85e4a735a5
Deepak Chethan <deepakchethan@outlook.com>
2018-10-17 18:30:11 +0530

    Added chain class

---
+++ src/models/chain.js
@@ -0,0 +1,19 @@
+const Block = require('./block');
+
+class Chain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  static getChain() {
+    return this.chain;
+  }
+
+  static addBlock(boondhi) {
+    const prevBlock = this.chain[this.chain.length - 1];
+    const block = Block.mineBlock(prevBlock, boondhi);
+    this.chain.push(block);
+  }
+}
+
+module.exports = Chain;
