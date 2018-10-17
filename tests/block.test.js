8488113f89fee4d831caabd42509b80a7f186cfa
Deepak Chethan <deepakchethan@outlook.com>
2018-10-17 18:16:48 +0530

    Refactored tests into test-chains

--- tests/block.test.js
+++ tests/block.test.js
@@ -1,31 +1,27 @@
 
 const Block = require('../src/models/block');
 
+describe('Block Tests', () => {
+  beforeEach(() => {});
 
-it('should create a block with given args', () => {
-  const block = new Block('foo', 'bar', 'zoo', 'baz');
-  expect(block.timeStamp).toBe('foo');
-  expect(block.prevHash).toBe('bar');
-  expect(block.currHash).toBe('zoo');
-  expect(block.boondhis).toBe('baz');
+  it('should create a block with given args', () => {
+    const block = new Block('foo', 'bar', 'zoo', 'baz');
+    expect(block.timeStamp).toBe('foo');
+    expect(block.prevHash).toBe('bar');
+    expect(block.currHash).toBe('zoo');
+    expect(block.boondhis).toBe('baz');
+  });
+  it('should have genesis block values equal to default', () => {
+    const block = Block.genesis();
+    expect(block.timeStamp).toBe('0000');
+    expect(block.prevHash).toBe('0000');
+    expect(block.currHash).toBe(Block.hash('0000', '0000', {}));
+    expect(block.boondhis).toEqual({});
+  });
+  it('should have create a first block with values provided', () => {
+    const block = Block.mineBlock(Block.genesis(), {});
+    expect(block.prevHash).toBe(Block.genesis().currHash);
+    expect(block.currHash).toBe(Block.hash(block.timeStamp, block.prevHash, block.boondhis));
+    expect(block.boondhis).toEqual({});
+  });
 });
-
-it('should have genesis block values equal to default', () => {
-  const block = Block.genesis();
-  expect(block.timeStamp).toBe('0000');
-  expect(block.prevHash).toBe('0000');
-  expect(block.currHash).toBe(Block.hash('0000', '0000', {}));
-  expect(block.boondhis).toEqual({});
-});
-
-it('should have create a first block with values provided', () => {
-  const block = Block.mineBlock(Block.genesis(), {});
-  expect(block.prevHash).toBe(Block.genesis().currHash);
-  expect(block.currHash).toBe(Block.hash(block.timeStamp, block.prevHash, block.boondhis));
-  expect(block.boondhis).toEqual({});
-});
-
-
-
-
-
