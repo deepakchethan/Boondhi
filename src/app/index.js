22961c13b23ceb60f20fec3b676131312552085a
Deepak Chethan <deepakchethan@outlook.com>
2018-10-17 21:01:39 +0530

    Added body-parser and mechanishm to add blocks
    fixed a typo

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Chain = require('../models/chain');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
-
 const app = express();
 const blockchain = new Chain();
+
+app.use(bodyParser.json());
+
+app.post('/mine', (req, res) => {
+  const block = blockchain.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
+});
 
 app.get('/blocks', (req, res) => {
   res.json(blockchain.chain);
